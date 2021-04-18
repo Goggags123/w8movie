6bb8fccd06d37cc7fd6a1133ab93eb46fb6e2d63
Goggags123 <wongsakorngag@gmail.com>
2021-04-18 10:39:54 +0700

    Resolve a little typo

--- src/components/DFA.js
+++ src/components/DFA.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment, useEffect } from "react";
+import React, { useState, Fragment } from "react";
 
 import * as go from "gojs";
 import { ReactDiagram } from "gojs-react";
