f2ea72ec7551d914ce430ff672029be92fa6e4a0
Goggags123 <wongsakorngag@gmail.com>
2021-04-20 14:37:17 +0700

    Adding Credit Popup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
-import Main from './containers/Main.js';
+import "./App.css";
+import Main from "./containers/Main.js";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Ellipse from "./components/Ellipse";
+import Loading from "./components/Loading";
+import Navbar from "./components/Navbar";
 function App() {
-  return (
-    <Main />
-  );
+	let [isLoading, setLoading] = useState(true);
+	return (
+		<div className="body">
+			<Ellipse className="ellipse1" />
+			<Ellipse className="ellipse2" />
+			<Ellipse className="ellipse3" />
+			<Router>
+				<Switch>
+					<Route path="/" exact>
+						{isLoading ? <Loading /> : <Navbar />}
+						<Main isLoading={isLoading} setLoading={setLoading} />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,70 +1,61 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import DFA from "../components/DFA";
 import Ellipse from "../components/Ellipse";
 import Navbar from "../components/Navbar";
 import Movie from "../components/Movie";
 import Loading from "../components/Loading";
-export default function Main() {
+export default function Main({ isLoading, setLoading }) {
 	let [input, setInput] = useState([]);
 	let [toggle, setToggle] = useState(true);
-	let [isLoading, setLoading] = useState(true);
 	let [currentState, setState] = useState(0);
 	useEffect(() => {
 		const lastInput = document.getElementById("input");
 		if (lastInput) lastInput.scrollIntoView();
 	}, [input]);
 	return (
-		<Fragment>
-			<div className="body">
-				<Ellipse className="ellipse1" />
-				<Ellipse className="ellipse2" />
-				<Ellipse className="ellipse3" />
-				{isLoading ? <Loading /> : <Navbar />}
-				<div className="allContainer">
-					<div className="dfa">
-						<div className="container">
-							<p>{"Input : "}</p>
-							<div className="text">
-								<div className="allInput">
-									{input.map((a, i) => {
-										return (
-											<div
-												id={i == input.length - 1 ? "input" : null}
-												className="inputComponent"
-												key={i}
-												style={
-													a == "Thai dub"
-														? { width: "4em" }
-														: a == "Add-on"
-														? { width: "3.2em" }
-														: {}
-												}
-											>
-												{a}
-											</div>
-										);
-									})}
-								</div>
-							</div>
+		<div className="allContainer">
+			<div className="dfa">
+				<div className="container">
+					<p>{"Input : "}</p>
+					<div className="text">
+						<div className="allInput">
+							{input.map((a, i) => {
+								return (
+									<div
+										id={i == input.length - 1 ? "input" : null}
+										className="inputComponent"
+										key={i}
+										style={
+											a == "Thai dub"
+												? { width: "4em" }
+												: a == "Add-on"
+												? { width: "3.2em" }
+												: {}
+										}
+									>
+										{a}
+									</div>
+								);
+							})}
 						</div>
-						<DFA
-							className="theDFA"
-							currentState={currentState}
-							setLoading={setLoading}
-							setState={setState}
-							toggle={toggle}
-							setToggle={setToggle}
-						/>
 					</div>
-					<Movie
-						setInput={setInput}
-						input={input}
-						setState={setState}
-						currentState={currentState}
-						toggle={toggle}
-					/>
 				</div>
+				<DFA
+					className="theDFA"
+					currentState={currentState}
+					setLoading={setLoading}
+					setState={setState}
+					toggle={toggle}
+					setToggle={setToggle}
+				/>
 			</div>
-		</Fragment>
+			<Movie
+				setInput={setInput}
+				input={input}
+				setState={setState}
+				currentState={currentState}
+				toggle={toggle}
+			/>
+		</div>
 	);
 }
